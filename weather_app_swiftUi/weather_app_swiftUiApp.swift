49859744c3d68d0d12554512493612b84c514d72
Arunava Basu <arunavabasudev@gmail.com>
2022-05-12 21:41:52 +0530

    ✅initial commit

---
+++ weather_app_swiftUi/weather_app_swiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_swiftUiApp.swift
+//  weather_app_swiftUi
+//
+//  Created by Arunava Basu on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_swiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
